36ff675a7f12f578c8c13eddf3484b92f171bf3e
Everton Rocha <imevertonrch@gmail.com>
2025-03-18 05:33:26 -0300

    refactory: adicionando getter e setter para o MapStruct

--- src/main/java/com/lab/ms_proposta/entity/Proposta.java
+++ src/main/java/com/lab/ms_proposta/entity/Proposta.java
@@ -1,11 +1,19 @@
 package com.lab.ms_proposta.entity;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.math.BigDecimal;
 
 @Entity
 @Table(name = "tb_proposta", schema = "public")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Proposta {
 
     @Id
--- src/main/java/com/lab/ms_proposta/entity/Usuario.java
+++ src/main/java/com/lab/ms_proposta/entity/Usuario.java
@@ -1,11 +1,19 @@
 package com.lab.ms_proposta.entity;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.math.BigDecimal;
 
 @Entity
 @Table(name = "tb_usuario", schema = "public")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Usuario {
 
     @Id
