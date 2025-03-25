d75f31d0e8f8e1591f436920f50020f4d7501b7d
Everton Rocha <imevertonrch@gmail.com>
2025-03-25 17:28:46 -0300

    refactory: removendo anotações do lombok

--- ms-analise-credito/src/main/java/com/lab/ms_analise_credito/domain/Proposta.java
+++ ms-analise-credito/src/main/java/com/lab/ms_analise_credito/domain/Proposta.java
@@ -1,12 +1,7 @@
 package com.lab.ms_analise_credito.domain;
 
-import lombok.Getter;
-import lombok.Setter;
-
 import java.math.BigDecimal;
 
-@Getter
-@Setter
 public class Proposta {
 
     private Long id;
@@ -17,4 +12,59 @@ public class Proposta {
     private String observacao;
     private Usuario usuario;
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public BigDecimal getValorSolicitado() {
+        return valorSolicitado;
+    }
+
+    public void setValorSolicitado(BigDecimal valorSolicitado) {
+        this.valorSolicitado = valorSolicitado;
+    }
+
+    public Short getPrazoPagamento() {
+        return prazoPagamento;
+    }
+
+    public void setPrazoPagamento(Short prazoPagamento) {
+        this.prazoPagamento = prazoPagamento;
+    }
+
+    public Boolean getAprovado() {
+        return aprovado;
+    }
+
+    public void setAprovado(Boolean aprovado) {
+        this.aprovado = aprovado;
+    }
+
+    public boolean isIntegrada() {
+        return integrada;
+    }
+
+    public void setIntegrada(boolean integrada) {
+        this.integrada = integrada;
+    }
+
+    public String getObservacao() {
+        return observacao;
+    }
+
+    public void setObservacao(String observacao) {
+        this.observacao = observacao;
+    }
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
+    }
 }
--- ms-analise-credito/src/main/java/com/lab/ms_analise_credito/domain/Usuario.java
+++ ms-analise-credito/src/main/java/com/lab/ms_analise_credito/domain/Usuario.java
@@ -1,13 +1,8 @@
 package com.lab.ms_analise_credito.domain;
 
-import lombok.Getter;
-import lombok.Setter;
-
 import java.math.BigDecimal;
 
 
-@Getter
-@Setter
 public class Usuario {
 
     private Long id;
@@ -17,4 +12,52 @@ public class Usuario {
     private String telefone;
     private BigDecimal renda;
 
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSobrenome() {
+        return sobrenome;
+    }
+
+    public void setSobrenome(String sobrenome) {
+        this.sobrenome = sobrenome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public BigDecimal getRenda() {
+        return renda;
+    }
+
+    public void setRenda(BigDecimal renda) {
+        this.renda = renda;
+    }
 }
