140208d0367dd77362ff788f83f0a5b5b8516d56
Everton Rocha <imevertonrch@gmail.com>
2025-03-25 23:34:54 -0300

    refactory: limpando código

--- ms-analise-credito/src/main/java/com/lab/ms_analise_credito/MsAnaliseCreditoApplication.java
+++ ms-analise-credito/src/main/java/com/lab/ms_analise_credito/MsAnaliseCreditoApplication.java
@@ -1,13 +1,15 @@
 package com.lab.ms_analise_credito;
 
+import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MsAnaliseCreditoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MsAnaliseCreditoApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication app = new SpringApplication(MsAnaliseCreditoApplication.class);
+        app.setBannerMode(Banner.Mode.OFF);
+        app.run(args);
+    }
 }
