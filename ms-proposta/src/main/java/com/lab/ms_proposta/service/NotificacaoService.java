f8efc2424546dad24c5dd5d65009282562bc3c85
Everton Rocha <imevertonrch@gmail.com>
2025-05-10 15:59:01 -0300

    refactory: removendo injeção via lombok

--- ms-proposta/src/main/java/com/lab/ms_proposta/service/NotificacaoService.java
+++ ms-proposta/src/main/java/com/lab/ms_proposta/service/NotificacaoService.java
@@ -1,15 +1,17 @@
 package com.lab.ms_proposta.service;
 
 import com.lab.ms_proposta.entity.Proposta;
-import lombok.AllArgsConstructor;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.stereotype.Service;
 
 @Service
-@AllArgsConstructor
 public class NotificacaoService {
 
-    private RabbitTemplate rabbitTemplate;
+    private final RabbitTemplate rabbitTemplate;
+
+    public NotificacaoService(RabbitTemplate rabbitTemplate) {
+        this.rabbitTemplate = rabbitTemplate;
+    }
 
     public void notificar(Proposta proposta, String exchange) {
         rabbitTemplate.convertAndSend(exchange, "", proposta);
