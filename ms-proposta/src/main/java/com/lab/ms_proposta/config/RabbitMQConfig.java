0542dab9f86aee4bdd2421725f786cff9a568dbe
Everton Rocha <imevertonrch@gmail.com>
2025-05-10 15:58:20 -0300

    refactory: definindo properties para as exchanges

--- ms-proposta/src/main/java/com/lab/ms_proposta/config/RabbitMQConfig.java
+++ ms-proposta/src/main/java/com/lab/ms_proposta/config/RabbitMQConfig.java
@@ -5,7 +5,7 @@ import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitAdmin;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
-import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.annotation.Bean;
@@ -14,30 +14,36 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class RabbitMQConfig {
 
+    @Value("${rabbitmq.proposta.pendente.exchange}")
+    private String exchangePropostaPendente;
+
+    @Value("${rabbitmq.proposta.concluida.exchange}")
+    private String exchangePropostaConcluida;
+
 
     @Bean
-    public Queue propostaPendenteAnaliseCredito() {
+    public Queue criarFilaPropostaPendenteMsAnaliseCredito() {
         final String QUEUE = "proposta-pendente.ms-analise-credito";
         return QueueBuilder.durable(QUEUE)
                 .build();
     }
 
     @Bean
-    public Queue propostaPendenteNotificacao() {
+    public Queue criarFilaPropostaPendenteMsNotificacao() {
         final String QUEUE = "proposta-pendente.ms-notificacao";
         return QueueBuilder.durable(QUEUE)
                 .build();
     }
 
     @Bean
-    public Queue propostaConcluidaProposta() {
+    public Queue criarFilaPropostaConcluidaMsProposta() {
         final String QUEUE = "proposta-concluida.ms-proposta";
         return QueueBuilder.durable(QUEUE)
                 .build();
     }
 
     @Bean
-    public Queue propostaConcluidaNotificacao() {
+    public Queue criarFilaPropostaConcluidaMsNotificacao() {
         final String QUEUE = "proposta-concluida.ms-notificacao";
         return QueueBuilder.durable(QUEUE)
                 .build();
@@ -55,21 +61,40 @@ public class RabbitMQConfig {
 
     @Bean
     public FanoutExchange criaFanoutExchangePropostaPendente() {
-        return ExchangeBuilder.fanoutExchange("proposta-pendente.ex").build();
+        return ExchangeBuilder.fanoutExchange(exchangePropostaPendente).build();
+    }
+
+    @Bean
+    public FanoutExchange criaFanoutExchangePropostaConcluida() {
+        return ExchangeBuilder.fanoutExchange(exchangePropostaConcluida).build();
     }
 
     @Bean
     public Binding bindingPropostaPendenteMsAnaliseCredito() {
         return BindingBuilder
-                .bind(propostaPendenteAnaliseCredito())
+                .bind(criarFilaPropostaPendenteMsAnaliseCredito())
                 .to(criaFanoutExchangePropostaPendente());
     }
 
     @Bean
     public Binding bindingPropostaPendenteMsNotificacao() {
         return BindingBuilder
-                .bind(propostaPendenteNotificacao())
+                .bind(criarFilaPropostaPendenteMsNotificacao())
                 .to(criaFanoutExchangePropostaPendente());
+    }
+
+    @Bean
+    public Binding bindingPropostaConcluidaMsProposta() {
+        return BindingBuilder
+                .bind(criarFilaPropostaConcluidaMsProposta())
+                .to(criaFanoutExchangePropostaConcluida());
+    }
+
+    @Bean
+    public Binding bindingPropostaConcluidaMsNotificacao() {
+        return BindingBuilder
+                .bind(criarFilaPropostaConcluidaMsNotificacao())
+                .to(criaFanoutExchangePropostaConcluida());
     }
 
     @Bean
