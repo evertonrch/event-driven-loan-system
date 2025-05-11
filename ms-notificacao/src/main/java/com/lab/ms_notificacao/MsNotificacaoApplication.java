c373bc2c6746eb0048f6ca8726b0314e35a36c4e
Everton Rocha <imevertonrch@gmail.com>
2025-05-11 16:38:58 -0300

    refactory: limpando classe

--- ms-notificacao/src/main/java/com/lab/ms_notificacao/MsNotificacaoApplication.java
+++ ms-notificacao/src/main/java/com/lab/ms_notificacao/MsNotificacaoApplication.java
@@ -1,10 +1,8 @@
 package com.lab.ms_notificacao;
 
 import org.springframework.boot.Banner;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class MsNotificacaoApplication {
@@ -13,15 +11,5 @@ public class MsNotificacaoApplication {
         SpringApplication app = new SpringApplication(MsNotificacaoApplication.class);
         app.setBannerMode(Banner.Mode.OFF);
         app.run(args);
-    }
-
-    @Bean
-    public CommandLineRunner commandLineRunner() {
-        return args -> {
-            final var token = System.getenv("TWILIO_AUTH_TOKEN");
-            final var sid = System.getenv("TWILIO_SID");
-            System.out.println("TOKEN : " + token);
-            System.out.println("SID: " + sid);
-        };
     }
 }
