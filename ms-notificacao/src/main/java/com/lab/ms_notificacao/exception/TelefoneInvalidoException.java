75f3f1df6a97d266d22257f88bf3859dc93a1b1b
Everton Rocha <imevertonrch@gmail.com>
2025-05-11 20:26:45 -0300

    feat: validando número de telefone

---
+++ ms-notificacao/src/main/java/com/lab/ms_notificacao/exception/TelefoneInvalidoException.java
@@ -0,0 +1,8 @@
+package com.lab.ms_notificacao.exception;
+
+public class TelefoneInvalidoException extends RuntimeException {
+
+    public TelefoneInvalidoException(String msg) {
+        super(msg);
+    }
+}
---
+++ ms-notificacao/src/main/java/com/lab/ms_notificacao/rule/impl/ValidaTelefoneRule.java
@@ -0,0 +1,22 @@
+package com.lab.ms_notificacao.rule.impl;
+
+import com.lab.ms_notificacao.exception.TelefoneInvalidoException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import java.util.regex.Pattern;
+
+@Component
+public class ValidaTelefoneRule {
+
+    private static final Logger LOG = LoggerFactory.getLogger(ValidaTelefoneRule.class);
+    private static final Pattern REGEX = Pattern.compile("^55\\d{2}9\\d{8}$");
+
+    public void validar(String telefone) {
+        if(!REGEX.matcher(telefone).matches()) {
+            LOG.error("formato inválido: {}", telefone);
+            throw new TelefoneInvalidoException("O telefone informado está inválido.");
+        }
+    }
+}
